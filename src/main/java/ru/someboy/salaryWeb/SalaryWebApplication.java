c2fdf23eb19753bcc1a9ba21c815a6e6148c36ec
someboygit <someboygit@gmail.com>
2024-03-12 14:17:54 +0300

    Создан проект

---
+++ src/main/java/ru/someboy/salaryWeb/SalaryWebApplication.java
@@ -0,0 +1,13 @@
+package ru.someboy.salaryWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/someboy/salaryWeb/SalaryWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.someboy.salaryWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
